d9b884c2590d2d8e78d9593175f39197f779b024
wellington <wellingtmax@gmail.com>
2025-08-05 22:20:18 -0300

    fa

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 import tabela2024 from './tabela.js';
+
 import express from 'express';
 
 const app = express();
+const PORT = 300
 
 app.get('/', (requisicao, resposta) => {
-    resposta.send(tabela2024)
+    resposta.status(200).send(tabela2024);
 })
 
-app.listen(300, () => console.log('servidor rodando com sucesso'));
+app.get('/:sigla', (requisicao, resposta) => {
+    const siglaInformada = requisicao.params.sigla.toLocaleUpperCase();
+    const time = tabela2024.find((infoTime) => infoTime.sigla === siglaInformada);
+    if(!time /*alternativa ->(time === undefined)<- */){ // undefined -> se comporta com falso (not(!time)) => é verdaddeiro
+        resposta.status(404)
+        .send(
+            'Time Não encontrado, Pode ser que esse time não faz parte desse Brasileirão!!'
+        )
+        return;
+    }
+    resposta.status(200).send(time);
+});
+
+app.put('/criar', (req, res) => {
+    const siglaInformada = req.params.siglas.toLocaleUpperCase();
+    const solo = siglaInformada.fetch((m) => {
+       
+    })
+})
+
+
+
+app.listen(PORT, () => console.log(`✅ servidor rodando com sucesso na porta: http://localhost:${PORT}`));
